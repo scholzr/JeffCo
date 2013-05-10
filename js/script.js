471825e6148fa51a8b643fe4eef3e7ea131e6bd5
Ryan Scholz <ryan@willamettews.com>
2013-05-09 19:44:56 -0700

    Changes

--- js/script.js
+++ js/script.js
@@ -3,58 +3,17 @@
 $.mobile.defaultPageTransition = "none"
 
 //Hydrant Page
-$("div#Hydrants").on("pageshow", function() {
+$(document).on("pageshow", "div#Hydrants", function() {
 	//Resize Map Div
 	$("#map-canvas").height($(window).height() - $("#HydrantHead").height() - $("#foot").height() - 35);
 	
 	document.getElementById("map-canvas").innerHTML = ""
-var map;
-  var mapOptions = {
-    zoom: 15,
-    //center: new google.maps.LatLng(44.6251, -121.1295),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+	RefreshMap();
+});
 
-  // Try HTML5 geolocation
-  if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = new google.maps.LatLng(position.coords.latitude,
-                                       position.coords.longitude);
-
-      map.setCenter(pos);
-	  
-    }, function() {
-      handleNoGeolocation(true);
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleNoGeolocation(false);
-  }
-
-var green = 'icons/green.fw.png';
-var yellow = 'icons/yellow.fw.png';
-var red = 'icons/red.fw.png';
-  var myLatLng = new google.maps.LatLng(44.6251, -121.1295);
-  var beachMarker = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      icon: green
-  });
-  var myLatLng = new google.maps.LatLng(44.6261, -121.1295);
-  var beachMarker = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      icon: yellow
-  });
-  var myLatLng = new google.maps.LatLng(44.6271, -121.1295);
-  var beachMarker = new google.maps.Marker({
-      position: myLatLng,
-      map: map,
-      icon: red
-  });
-
+$(window).resize(function() {
+	//Resize Map Div
+	$("#map-canvas").height($(window).height() - $("#HydrantHead").height() - $("#foot").height() - 35);	
 });
 
 function handleNoGeolocation(errorFlag) {
@@ -68,7 +27,7 @@ function handleNoGeolocation(errorFlag) {
 }
 
 //Location Page
-$("div#Location").on("pageshow", function() {
+$(document).on("pageshow", "div#Location", function() {
 	navigator.geolocation.getCurrentPosition(GeoSuccess, GeoError, {enableHighAccuracy: true});
 });
 
@@ -76,6 +35,13 @@ function PositionRefresh() {
 	navigator.geolocation.getCurrentPosition(GeoSuccess, GeoError, {enableHighAccuracy: true});
 }
 function GeoSuccess(position) {
+	$.mobile.loading( "show", {
+		text: "Loading",
+		textVisible: true,
+		theme: "a",
+		html: ""
+	});
+	
 	var Lat = position.coords.latitude;
 	var Lon = position.coords.longitude;
 	var Alt = position.coords.altitude;
@@ -87,18 +53,21 @@ function GeoSuccess(position) {
 	LAc = LAc * 3.28084;
 	AAc = AAc * 3.28084;
 	
-	document.getElementById("LatitudeSpan").innerHTML = LocationFormatter.decimalLatToDMS(Lat, LocationFormatter.SOUTH);
-	document.getElementById("LongitudeSpan").innerHTML = LocationFormatter.decimalLongToDMS(Lon, LocationFormatter.WEST);
-	document.getElementById("LAccuracySpan").innerHTML = Math.round(LAc);
-	document.getElementById("AltitudeSpan").innerHTML = Math.round(Alt);
-	document.getElementById("AAccuracySpan").innerHTML = Math.round(AAc);
+	setTimeout(function(){
+		document.getElementById("LatitudeSpan").innerHTML = LocationFormatter.decimalLatToDMS(Lat, LocationFormatter.SOUTH);
+		document.getElementById("LongitudeSpan").innerHTML = LocationFormatter.decimalLongToDMS(Lon, LocationFormatter.WEST);
+		document.getElementById("LAccuracySpan").innerHTML = Math.round(LAc);
+		document.getElementById("AltitudeSpan").innerHTML = Math.round(Alt);
+		document.getElementById("AAccuracySpan").innerHTML = Math.round(AAc);
+		$.mobile.loading( "hide" );
+	},500);
 }
 
 function GeoError() {
 	alert('Error!');
 }
 
-$("div#viewer").on("pageshow", function() {
+$(document).on("pageshow", "div#viewer", function() {
 	//Resize Map Div
 	$("#viewerIF").height($(window).height() - $("#ViewerHead").height() - 35);
 });
@@ -108,28 +77,60 @@ function OpenPage(url) {
 }
 
 function RefreshMap() {
-var map;
-  var mapOptions = {
-    zoom: 15,
-    //center: new google.maps.LatLng(44.6251, -121.1295),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-
-	  // Try HTML5 geolocation
-  if(navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var pos = new google.maps.LatLng(position.coords.latitude,
-                                       position.coords.longitude);
-
-      map.setCenter(pos);
-	  
-    }, function() {
-      handleNoGeolocation(true);
-    });
-  } else {
-    // Browser doesn't support Geolocation
-    handleNoGeolocation(false);
-  }
+	$.mobile.loading( "show", {
+		text: "Loading",
+		textVisible: true,
+		theme: "a",
+		html: ""
+	});
+	
+	setTimeout(function(){
+		var map;
+		  var mapOptions = {
+			zoom: 15,
+			//center: new google.maps.LatLng(44.6251, -121.1295),
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+		  };
+		  map = new google.maps.Map(document.getElementById('map-canvas'),
+			  mapOptions);
+		
+			  // Try HTML5 geolocation
+		  if(navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(function(position) {
+			  var pos = new google.maps.LatLng(position.coords.latitude,
+											   position.coords.longitude);
+		
+			  map.setCenter(pos);
+			  
+			}, function() {
+			  handleNoGeolocation(true);
+			});
+		  } else {
+			// Browser doesn't support Geolocation
+			handleNoGeolocation(false);
+		  }
+		var green = 'icons/green.fw.png';
+		var yellow = 'icons/yellow.fw.png';
+		var red = 'icons/red.fw.png';
+		  var myLatLng = new google.maps.LatLng(44.6251, -121.1295);
+		  var beachMarker = new google.maps.Marker({
+			  position: myLatLng,
+			  map: map,
+			  icon: green
+		  });
+		  var myLatLng = new google.maps.LatLng(44.6261, -121.1295);
+		  var beachMarker = new google.maps.Marker({
+			  position: myLatLng,
+			  map: map,
+			  icon: yellow
+		  });
+		  var myLatLng = new google.maps.LatLng(44.6271, -121.1295);
+		  var beachMarker = new google.maps.Marker({
+			  position: myLatLng,
+			  map: map,
+			  icon: red
+		  });
+		$.mobile.loading( "hide" );
+	},500);
+	
 };
